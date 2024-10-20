a49c28151bdb7d37f3c9beeaba1de7850513d7f1
harisheoran <harisheoran@protonmail.com>
2024-10-20 15:46:14 +0530

    added cors middleware

--- api-server/cmd/web/routes.go
+++ api-server/cmd/web/routes.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func (app *app) routes() http.Handler {
 
 	router := gin.Default()
+	// Apply CORS middleware with configuration
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://your-frontend.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,           // Enable cookies and credentials
+		MaxAge:           12 * time.Hour, // Cache preflight response
+	}))
 
 	router.GET("/health", app.healthHandler)
 	router.POST("/deploy", app.requireAuthenticatedUserMiddleware(app.deploymentHandler))
